31a25cbc3fd75cb60b47167efcfa9fd8cef675c5
joshibhaskar684 <joshibhaskar684@gmail.com>
2026-03-05 18:04:33 +0530

    proxy page created

---
+++ src/proxy.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+export async function proxy(request){
+    const UserToken=request.cookies.get("usertoken")?.value;
+ const url = request.nextUrl.clone();
+
+ const {pathname} = url;
+ if(pathname.startsWith("/student")){
+
+    if(!UserToken){
+  return NextResponse.redirect(new URL("/", request.url));
+    }
+    else{
+        return NextResponse.next();
+    }
+ }
+
+    return NextResponse.next();
+}
+
+
+export const config={
+  matcher:["/admin/:path*","/student/:path*","/seller-login"],
+  
+}
