b84db2310670422fc90001a0f2c7f3f5681fed96
joshibhaskar684 <joshibhaskar684@gmail.com>
2026-03-09 19:00:32 +0530

    user login register logic added redux

--- src/redux-store/user/action.js
+++ src/redux-store/user/action.js
@@ -1,22 +1,115 @@
+import Cookies from "js-cookie";
+import {
+     LOGIN_USER_REQUEST,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAILURE,
+    LOGOUT_USER_REQUEST,
+    LOGOUT_USER_SUCCESS,
+    LOGOUT_USER_FAILURE,
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCCESS,
+    REGISTER_USER_FAILURE,
+    UPDATE_USER_REQUEST,
+    UPDATE_USER_SUCCESS,
+    UPDATE_USER_FAILURE,
+    DELETE_USER_REQUEST,
+    DELETE_USER_SUCCESS,
+    DELETE_USER_FAILURE,
+    GET_USER_REQUEST,
+    GET_USER_SUCCESS,
+    GET_USER_FAILURE
+
+
+
+
+
+ } from"./actiontype";
 import { toast } from "react-toastify";
-import { GET_USER_FAILURE, GET_USER_REQUEST, GET_USER_SUCCESS } from "./actiontype";
 import axios from "axios";
 
 
-const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL ;
+ const backendUrl=process.env.NEXT_PUBLIC_BACKEND_URL;
 
-export const getUserRequest = (data) =>async (dispatch) => {
-
-    dispatch({ type: GET_USER_REQUEST });
+ export const LoginUser=(data)=>async(dispatch)=>{
+    dispatch({type:LOGIN_USER_REQUEST});
+    const router=data.router
     try{
-        const response =await axios(`${backendUrl}/user/getuser`);
-        
-        dispatch({ type: GET_USER_SUCCESS, payload: response.data });
+        const res= await axios.post(`${backendUrl}/auth/login`,data ,{
+  withCredentials: true
+});
+     router.push("/");
+        dispatch({type:LOGIN_USER_SUCCESS,payload:res.data});
     }
-        catch(error){   
-            toast.error("Please try again."+error.message)
-        dispatch({ type: GET_USER_FAILURE, payload: error.message });
-        
+    catch(err){
+        toast.error(err.response.data.message);
+        dispatch({type:LOGIN_USER_FAILURE,payload:err.message});
     }
+ }
+ export const LogoutUser = (data) => async (dispatch) => {
+  dispatch({ type: LOGOUT_USER_REQUEST });
+  try {
+    Cookies.remove("usertoken");  // <-- removes cookie
+    dispatch({ type: LOGOUT_USER_SUCCESS, payload: data });
+  } catch (err) {
+    toast.error(err.message);
+    dispatch({ type: LOGOUT_USER_FAILURE, payload:err.message });
+  }
+};
 
-}
+  export const RegisterUser=(data)=>async(dispatch)=>{
+    dispatch({type:REGISTER_USER_REQUEST});
+    try{
+        const res= await axios.post(`${backendUrl}/auth/signup`,data, {
+  withCredentials: true
+});
+        dispatch({type:REGISTER_USER_SUCCESS,payload:res.data});
+    }
+    catch(err){
+
+    toast.error(err.message);
+        dispatch({type:REGISTER_USER_FAILURE,payload:err.message});
+    }
+ }
+
+   export const UpdateUser=(data)=>async(dispatch)=>{
+    dispatch({type:UPDATE_USER_REQUEST});
+    try{
+        const res= await axios.post(`${backendUrl}/user/login`,data);
+        const data=res.data;
+        dispatch({type:UPDATE_USER_SUCCESS,payload:data});
+    }
+    catch(err){
+        dispatch({type:UPDATE_USER_FAILURE,payload:err.message});
+    }
+ }
+
+    export const GetUser=(data)=>async(dispatch)=>{
+        
+        const token=data.token;
+    dispatch({type:GET_USER_REQUEST});
+    try{
+      const res = await axios.get(`${backendUrl}/auth/user/profile`, {
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+});
+        const data=res.data;
+        dispatch({type:GET_USER_SUCCESS,payload:data});
+    }
+    catch(err){
+        dispatch({type:GET_USER_FAILURE,payload:err.message});
+    }
+ }
+
+
+   export const DeleteUser=(data)=>async(dispatch)=>{
+    dispatch({type:DELETE_USER_REQUEST});
+    try{
+        const res= await axios.post(`${backendUrl}/user/login`,data);
+        const data=res.data;
+        dispatch({type:DELETE_USER_SUCCESS,payload:data});
+    }
+    catch(err){
+        dispatch({type:DELETE_USER_FAILURE,payload:err.message});
+    }
+ }
--- src/redux-store/user/reducer.js
+++ src/redux-store/user/reducer.js
@@ -1,32 +1,87 @@
-import { GET_USER_FAILURE, GET_USER_REQUEST, GET_USER_SUCCESS } from "./actiontype";
+import {
+    LOGIN_USER_REQUEST,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAILURE,
+    LOGOUT_USER_REQUEST,
+    LOGOUT_USER_SUCCESS,
+    LOGOUT_USER_FAILURE,
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCCESS,
+    REGISTER_USER_FAILURE,
+    UPDATE_USER_REQUEST,
+    UPDATE_USER_SUCCESS,
+    UPDATE_USER_FAILURE,
+    DELETE_USER_REQUEST,
+    DELETE_USER_SUCCESS,
+    DELETE_USER_FAILURE,
+    GET_USER_REQUEST,
+    GET_USER_SUCCESS,
+    GET_USER_FAILURE
+
+
+
+
+
+} from "./actiontype";
+
+
 
 const initialState = {
-  data:[],
-    isLoading: false,
     user: null,
+    data: [],
+    loading: false,
     error: null,
-    token: null
-}
 
-export const UserReducer = (state = initialState, action) => {
+}
+export default function UserReducer(state = initialState, action) {
+
     switch (action.type) {
 
-      case GET_USER_REQUEST:
-        return { ...state, isLoading: true };
+        case LOGIN_USER_REQUEST:
+        case REGISTER_USER_REQUEST:
+        case UPDATE_USER_REQUEST:
+        case DELETE_USER_REQUEST:
+            case LOGOUT_USER_REQUEST:
+        case GET_USER_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                error: null
+            }
 
-
+        case LOGOUT_USER_SUCCESS:
+        case REGISTER_USER_SUCCESS:
+        case UPDATE_USER_SUCCESS:
+        case DELETE_USER_SUCCESS:
+        case LOGIN_USER_SUCCESS:
+            return {
+                ...state,
+                data: action.payload,
+                loading: false,
+                error: null
+            }
         case GET_USER_SUCCESS:
-        return { ...state, isLoading: false, data: action.payload };
+            return {
+                ...state,
+                user: action.payload,
+                loading: false,
+                error: null
+            }
 
-
-        
-        case GET_USER_FAILURE:
-        return { ...state, isLoading: false, error: action.payload };
-
-      default:
-        return state;
+        case LOGIN_USER_FAILURE:
+            case REGISTER_USER_FAILURE:
+            case UPDATE_USER_FAILURE:
+            case DELETE_USER_FAILURE:
+            case LOGOUT_USER_FAILURE:
+            case GET_USER_FAILURE:
+                
+            return {
+                ...state,
+                user: null,
+                loading: false,
+                error: action.payload
+            }
+        default:
+            return state;
     }
 }
-
-
-
