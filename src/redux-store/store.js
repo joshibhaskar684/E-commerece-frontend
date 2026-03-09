f437db9ca38d93150e2e53a52fd651fd55d12e01
joshibhaskar684 <joshibhaskar684@gmail.com>
2026-03-09 19:18:28 +0530

    added new reducer

--- src/redux-store/store.js
+++ src/redux-store/store.js
@@ -1,15 +1,18 @@
-import { CartReducer } from "./cart/reducer";
-import { OrderReducer } from "./Order/reducer";
-import { ProductReducer } from "./products/reducer";
-import { UserReducer } from "./user/reducer";
+// import { CartReducer } from "./cart/reducer";
+// import { OrderReducer } from "./Order/reducer";
+// // import { ProductReducer } from "./products/reducer";
+// import { UserReducer } from "./user/reducer";
+
+import UserReducer from "./user/reducer";
 
 const { combineReducers, configureStore } = require("@reduxjs/toolkit");
 
 const rootReducer=combineReducers({
-user:UserReducer,
-ProductReducer:ProductReducer,
-CartReducer:CartReducer,
-OrderReducer:OrderReducer,
+    UserReducer:UserReducer,
+
+// ProductReducer:ProductReducer,
+// CartReducer:CartReducer,
+// OrderReducer:OrderReducer,
 
 });
 
