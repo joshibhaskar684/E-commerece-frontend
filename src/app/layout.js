588a000f2c167b344c115fe187f264f711de0359
joshibhaskar684 <joshibhaskar684@gmail.com>
2026-03-30 20:06:30 +0530

    font added

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+<head>
+  <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Jost:ital,wght@0,100..900;1,100..900&family=ZCOOL+QingKe+HuangYou&display=swap" rel="stylesheet"/>
+  </head>
+
       <body
         className={` antialiased`}
       >
