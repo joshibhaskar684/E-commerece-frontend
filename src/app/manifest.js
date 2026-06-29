8eb1c935b7d91595102f91549c4c4a78d8ab96f2
joshibhaskar684 <joshibhaskar684@gmail.com>
2026-06-29 05:51:07 +0530

    manifest.js file added to make it PWA

---
+++ src/app/manifest.js
@@ -0,0 +1,23 @@
+export default function manifest() {
+  return {
+    name: 'Quick | Modern Full Stack Ecommerce Platform',
+    short_name: 'Quick',
+    description: 'A powerful full-stack ecommerce platform built by Bhaskar Joshi.',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/QuickLogo.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/QuickLogo.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
